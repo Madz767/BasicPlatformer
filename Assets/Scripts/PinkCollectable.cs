2092d02b4feadb13616ff454b2f9eef0605e4434
Todd Coleman <102484081+Madz767@users.noreply.github.com>
2025-09-10 16:44:24 -0400

    Fine tuned player movement
    Added collectable as well

---
+++ Assets/Scripts/PinkCollectable.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PinkCollectable : MonoBehaviour
+{
+    string test;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public string getTestString()
+    {
+        test = "Hello from pink collectable";
+        return test;
+    }
+
+
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,8 @@ public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rb;
     private float inputHorizontal;
-    private float inputVertical;
+    private int maxNumJumps = 1;
+    private int numJumps;
     public float jumpPower;
     public float movementSpeed;
 
@@ -13,6 +14,7 @@ public class PlayerMovement : MonoBehaviour
     {
         //Debug.Log("Hello from PlayerMovement");
         rb = GetComponent<Rigidbody2D>();
+        numJumps = maxNumJumps;
     }
 
     // Update is called once per frame
@@ -26,18 +28,55 @@ public class PlayerMovement : MonoBehaviour
     {
         // used to move the player from left to right based on left to right
         inputHorizontal = Input.GetAxisRaw("Horizontal");
+        flipPlayerSprite(inputHorizontal);
         rb.linearVelocity = new Vector2(movementSpeed*inputHorizontal, rb.linearVelocity.y);
     }
 
     private void jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && numJumps == 1)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocityX, jumpPower);
-
+            numJumps = 0;
         }
 
     }
 
+    private void flipPlayerSprite(float inputHorizontal)
+    {
+        //this will be used to flip the sprites of the player
+        //works off of a 0 to 1 value to determine which way the player is facing
+        if (inputHorizontal > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+
+        }
+        else if(inputHorizontal < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+
+        //==============================================
+        //this allows for only one jump from the ground
+        //==============================================
+        if (col.gameObject.CompareTag("Ground"))
+        {
+            numJumps = maxNumJumps;
+        }
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Collectable"))
+        {
+            string fromPink = collision.gameObject.GetComponent<PinkCollectable>().getTestString();
+            Debug.Log(fromPink);
+        }
+    }
 
 }
